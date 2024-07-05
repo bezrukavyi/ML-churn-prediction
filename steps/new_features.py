7df09f9124fa8e459580459d1f85c2b9574249bf
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2024-07-05 10:54:40 +0300

    Refactoring bnum features. Keep only important features

--- steps/new_features.py
+++ steps/new_features.py
@@ -17,14 +17,14 @@ def merge_test_dpi_features(dataframe):
 
 
 def merge_train_bnum_features(dataframe):
-    with open("cache/project_step_4_train.pkl", "rb") as f:
+    with open("cache/project_step_4(2)_train.pkl", "rb") as f:
         bnum_df = pickle.load(f).drop(columns=["target"])
 
     return dataframe.merge(bnum_df, on="abon_id", how="left").fillna(0)
 
 
 def merge_test_bnum_features(dataframe):
-    with open("cache/project_step_4_test.pkl", "rb") as f:
+    with open("cache/project_step_4(2)_test.pkl", "rb") as f:
         bnum_df = pickle.load(f).drop(columns=["target"])
 
     return dataframe.merge(bnum_df, on="abon_id", how="left").fillna(0)
