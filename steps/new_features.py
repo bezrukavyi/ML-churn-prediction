a754bea4b273923283ff1d2297c3cb22ca1a6189
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2024-07-05 21:36:04 +0300

    Refactoring dpi features. Keep only important features

--- steps/new_features.py
+++ steps/new_features.py
@@ -3,14 +3,14 @@ import pdb
 
 
 def merge_train_dpi_features(dataframe):
-    with open("cache/project_step_3_train.pkl", "rb") as f:
+    with open("cache/project_step_3(short)_train.pkl", "rb") as f:
         dpi_df = pickle.load(f).drop(columns=["target"])
 
     return dataframe.merge(dpi_df, on="abon_id", how="left").fillna(0)
 
 
 def merge_test_dpi_features(dataframe):
-    with open("cache/project_step_3_test.pkl", "rb") as f:
+    with open("cache/project_step_3(short)_test.pkl", "rb") as f:
         dpi_df = pickle.load(f).drop(columns=["target"])
 
     return dataframe.merge(dpi_df, on="abon_id", how="left").fillna(0)
